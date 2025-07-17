f5f0aac4a7acad570944a8380c48672c210dcd24
NBKUSA <sridevinukella@gmail.com>
2025-07-18 02:45:08 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>LLOYDS MORGAN Pay</title>
+      </Head>
+      <main>
+        <h1>Welcome to LLOYDS MORGAN Pay Terminal</h1>
+        <p>Please log in to continue.</p>
+      </main>
+    </div>
+  );
+}
